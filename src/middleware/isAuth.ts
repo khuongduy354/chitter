8a052113dd84d5b671839e246929bdd8b930c876
khuongduy354 <42113313+khuongduy354@users.noreply.github.com>
2024-01-20 20:56:02 +0700

    fix: header access token instead of body

--- src/middleware/isAuth.ts
+++ src/middleware/isAuth.ts
@@ -6,12 +6,20 @@ export const isAuth = async (
   res: Response,
   next: NextFunction
 ) => {
-  const accessToken = req.body.accessToken;
+  const accessToken = req.header("Authorization");
   const supa_user = (await supabase.auth.getUser(accessToken)).data.user;
   if (supa_user != null) {
+    const { data } = await supabase
+      .from("User")
+      .select()
+      .eq("email", supa_user.email);
+    if (!data) return res.status(404).json({ message: "User not found" });
+
     req.user = supa_user;
+    req.user.id = data[0].id;
     next();
   } else {
+    console.log("Unauthorized");
     return res.status(401).json({ message: "Unauthorized" });
   }
 };
