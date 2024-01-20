4c5daeddcb6e3d200295584a4fdbdad9277336a9
khuongduy354 <42113313+khuongduy354@users.noreply.github.com>
2024-01-20 20:55:52 +0700

    feat: friend overhaul!

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -11,6 +11,74 @@ const searchFriend = async (req: Request, res: Response) => {
     throw err;
   }
 };
+const addFriend = async (req: Request, res: Response) => {
+  try {
+    // check if request is available
+    const { friendId } = req.body;
+    const { data: reqData } = await supabase
+      .from("FriendRequest")
+      .select()
+      .eq("to", req.user.id)
+      .eq("from", friendId);
+
+    if (reqData) {
+      // add friend
+      const { friendId } = req.body;
+      const { error } = await supabase
+        .from("Friend")
+        .insert({ user1_id: req.user.id, user2_id: friendId });
+
+      if (error) res.status(404).json({ message: "Cannot add friend" });
+      else {
+        //  try to delete request
+        const { error } = await supabase
+          .from("FriendRequest")
+          .delete()
+          .eq("to", req.user.id)
+          .eq("from", friendId);
+
+        if (error)
+          res.status(404).json({ message: "Cannot delete friend request" });
+        else res.status(200).json({ message: "Friend added" });
+      }
+    } else {
+      res.status(404).json({ message: "Friend request not found" });
+    }
+  } catch (err) {
+    throw err;
+  }
+};
+const sendFriendRequest = async (req: Request, res: Response) => {
+  try {
+    const { friendId } = req.body;
+    // check user exists
+    const { data: userData } = await supabase
+      .from("User")
+      .select()
+      .eq("id", friendId);
+    if (!userData) return res.status(404).json({ message: "User not found" });
+
+    const { error } = await supabase
+      .from("FriendRequest")
+      .insert({ from: req.user.id, to: friendId });
+    if (error) res.status(404).json({ message: "User not found" });
+    else res.status(200).json({ message: "Friend request sent" });
+  } catch (err) {
+    throw err;
+  }
+};
+const getFriendRequestsReceived = async (req: Request, res: Response) => {
+  try {
+    const { data } = await supabase
+      .from("FriendRequest")
+      .select()
+      .eq("to", req.user.id);
+    if (data) res.status(200).json({ friendRequests: data });
+    else res.status(404).json({ message: "Error" });
+  } catch (err) {
+    throw err;
+  }
+};
 const getMe = async (req: Request, res: Response) => {
   // database query to get user
   try {
@@ -24,10 +92,14 @@ const getMe = async (req: Request, res: Response) => {
     throw err;
   }
 };
-const getFriends = (req: Request, res: Response) => {
+const getFriends = async (req: Request, res: Response) => {
   try {
-    let friends = [];
-    res.status(200).json({ friends });
+    const { data } = await supabase.rpc("query_friend", {
+      email: req.user.email,
+    });
+    console.log(data);
+    if (data) res.status(200).json({ friends: data });
+    else res.status(404).json({ message: "Error" });
   } catch (error) {}
 };
 
@@ -38,4 +110,12 @@ const getGroups = (req: Request, res: Response) => {
   } catch (error) {}
 };
 
-export const UserController = { getGroups, getFriends, getMe, searchFriend };
+export const UserController = {
+  getGroups,
+  getFriends,
+  getMe,
+  searchFriend,
+  addFriend,
+  getFriendRequestsReceived,
+  sendFriendRequest,
+};
--- src/routers/user.route.ts
+++ src/routers/user.route.ts
@@ -5,6 +5,13 @@ import { isAuth } from "../middleware/isAuth";
 const UserRoute = Router();
 
 UserRoute.get("/me/friends", isAuth, UserController.getFriends);
+UserRoute.get(
+  "/me/friend/requests/received",
+  isAuth,
+  UserController.getFriendRequestsReceived
+);
+UserRoute.post("/me/friend/request", isAuth, UserController.sendFriendRequest);
+UserRoute.post("/me/friend/add", isAuth, UserController.addFriend);
 UserRoute.get("/me/groups", isAuth, UserController.getGroups);
 UserRoute.get("/me", isAuth, UserController.getMe);
 UserRoute.get("/user", UserController.searchFriend);
