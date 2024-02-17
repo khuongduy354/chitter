bbb548b86eb480a32404254d956b048b09902547
khuongduy354 <42113313+khuongduy354@users.noreply.github.com>
2024-02-17 16:33:30 +0700

    feat: redefine theme data structure

--- src/middleware/sanitizeThemePayload.ts
+++ src/middleware/sanitizeThemePayload.ts
@@ -1,32 +1,59 @@
 import { NextFunction, Request, Response } from "express";
 
+const sampleTheme = {
+  themeName: "sample",
+  sender_msg_color: "#000000",
+  receiver_msg_color: "#000000",
+  bg: {
+    bgColor: "#000000",
+    layers: [
+      {
+        img_url: "https://example.com",
+        speed: 1,
+        translateYMin: 0,
+        translateYMax: 0,
+        margin: 0,
+      },
+    ],
+    divider: {
+      height: 0,
+      max_height: 0,
+      color: "#000000",
+    },
+  },
+};
 export const sanitizeThemePayload = (
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
-  const payload = req.body;
-  const { layers, themeName, divider, sender_msg_color, receiver_msg_color } =
-    payload;
+  // general sanitize
+  const { bg, themeName, sender_msg_color, receiver_msg_color } =
+    req.body.theme;
   if (
-    layers === undefined ||
+    bg === undefined ||
     themeName === undefined ||
-    divider === undefined ||
     sender_msg_color === undefined ||
     receiver_msg_color === undefined
   ) {
     return res.status(400).json({ message: "Invalid payload" });
   }
+
+  // sanitize bg
+  const { layers, divider, bgColor } = bg;
+
+  if (bgColor === undefined || divider === undefined || layers === undefined)
+    return res.status(400).json({ message: "Invalid bg" });
+
   //sanitize layers
   if (Array.isArray(layers) === false)
     return res.status(400).json({ message: "Invalid layers" });
   if (layers.length > 10) {
     return res.status(400).json({ message: "Max layers is 10" });
   }
-  for (let layer in layers) {
+  for (let layer of layers) {
     if (
-      layer["img_url"] !== undefined ||
-      layer["speed"] === undefined ||
+      layer["imgUrl"] === undefined ||
       layer["translateYMin"] === undefined ||
       layer["translateYMax"] === undefined ||
       layer["margin"] === undefined
@@ -37,7 +64,7 @@ export const sanitizeThemePayload = (
   // sanitize divider
   if (
     divider["height"] === undefined ||
-    divider["max_height"] === undefined ||
+    divider["maxHeight"] === undefined ||
     divider["color"] === undefined
   ) {
     return res.status(400).json({ message: "Invalid divider" });
