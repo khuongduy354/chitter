a6355db703b012ccc66f30aaa86dc1f46c25688b
khuongduy354 <42113313+khuongduy354@users.noreply.github.com>
2025-05-04 16:12:23 +0700

    chore: cleanup

--- Makefile
+++ Makefile
@@ -1,11 +1,20 @@
 run-docker-compose: 
 	docker compose -f ./Docker/docker-dev-compose.yml up
 
+run-kafka-compose:
+	docker compose -f ./Docker/kafka/kafka-compose.yml up
+
 stop-docker-compose: 
 	docker compose -f ./Docker/docker-dev-compose.yml down
 
-run-docker:  
-	docker run --env-file .env -p 8000:8000 selfhost-meet-chitter-be
+# run-docker:  
+# 	docker run --env-file .env -p 8000:8000 selfhost-meet-chitter-be
 
-build-docker:
-	docker build -t chitter/selfhost-meet-be:1.0 -f ./Docker/Dockerfile .
+
+build_all: build-docker-express build-docker-go
+
+build-docker-express:
+	docker build -t chitter/express:1.0 -f ./Docker/express/Dockerfile .
+
+build-docker-go:
+	docker build -t chitter/gomsq:1.0 -f ./Docker/gomsq/Dockerfile ./gopgms
--- src/helper/kafka.ts
+++ src/helper/kafka.ts
@@ -1,42 +1,74 @@
-import { EachMessagePayload, Kafka, Message } from "kafkajs"; 
-const kafkaurl = process.env.KAFKA_URL as string === undefined ? "localhost:9092" : process.env.KAFKA_URL as string;
-const kafka = new Kafka({
-  clientId: "my-app2",
-  brokers: [kafkaurl],
-});
+import { Consumer, EachMessagePayload, Kafka, Producer } from "kafkajs";
+// connect kafka on app startup
+
+let producer: null | Producer = null;
+let g1Consumer: null | Consumer = null;
+async function connectKafka() {
+  try {
+    const kafkaurl =
+      (process.env.KAFKA_URL as string) === undefined
+        ? "localhost:9092"
+        : (process.env.KAFKA_URL as string);
+    const kafka = new Kafka({
+      clientId: "my-app2",
+      brokers: [kafkaurl],
+    });
+
+    producer = kafka.producer();
+    await producer.connect();
+    console.log("Kafka producer connected!");
+
+    const consumer = kafka.consumer({ groupId: "g1" });
+    await consumer.connect();
+  } catch (err) {
+    throw err;
+  }
+}
+
 export type KafkaConsumerCallback = (payload: {
   topic: string;
   partition: number;
   message: any;
-}) => void; 
+}) => void;
 
-export async function sendToKafka(topic: string, messages: [string], cb: Function) {
-  const producer = kafka.producer();
-  await producer.connect(); 
+export async function sendToKafka(
+  topic: string,
+  messages: [string],
+  cb: Function
+) {
+  if (!producer) {
+    await connectKafka();
+  }
   const payload = {
     topic,
     messages: messages.map((message) => {
       return { value: message };
     }),
-  }
-  await producer.send(payload); 
-  cb(payload); 
-  await producer.disconnect();
+  };
+
+  if (!producer) return;
+  await producer.send(payload);
+  cb(payload);
 }
 
 export async function subscribeTopic(topic: string, cb: KafkaConsumerCallback) {
-  const consumer = kafka.consumer({ groupId: "g1" });
-  await consumer.connect();
-  await consumer.subscribe({ topic, fromBeginning: true });
-  await consumer.run({
-    eachMessage: async (payload: EachMessagePayload) => {  
-      let _payload ={ 
+  if (!g1Consumer) {
+    await connectKafka();
+  }
+
+  if (!g1Consumer) return;
+  await g1Consumer.subscribe({ topic, fromBeginning: true });
+  await g1Consumer.run({
+    eachMessage: async (payload: EachMessagePayload) => {
+      let _payload = {
         ...payload,
-        message: parseKafkaStringToMsgJson(payload.message.value?.toString() as string),
-      } 
+        message: parseKafkaStringToMsgJson(
+          payload.message.value?.toString() as string
+        ),
+      };
       cb(_payload);
     },
-  }); 
+  });
 }
 export const parseMsgJsonToKafkaString = (msg: {
   content: string;
@@ -45,8 +77,8 @@ export const parseMsgJsonToKafkaString = (msg: {
 }) => {
   return Object.values(msg).join("%");
 };
-  
-export const parseKafkaStringToMsgJson = (msg: string):any => {
+
+export const parseKafkaStringToMsgJson = (msg: string): any => {
   const [content, from, room] = msg.split("%");
   return { content, from, room };
-}
+};
